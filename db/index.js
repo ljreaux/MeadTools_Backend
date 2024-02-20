d049a595d43cad0a5936f5129209a22f40bc3f8b
Larry Reaux <larryreaux@gmail.com>
2024-02-19 20:01:32 -0600

    setup initial seed of db

---
+++ db/index.js
@@ -0,0 +1,422 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgres://localhost:5432/meadtools-dev",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+async function createUser({
+  firstName = "",
+  lastName = "",
+  username,
+  email,
+  password,
+  role,
+}) {
+  if (!role) role = "user";
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    INSERT INTO users (first_name, last_name, username, email, password, role)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING *;
+    `,
+      [firstName, lastName, username, email, password, role]
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      UPDATE users
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+    `,
+      Object.values(fields)
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllUsers() {
+  try {
+    const { rows: users } = await client.query(`
+      SELECT * FROM users;
+    `);
+    return users;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUser(id) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+      SELECT * FROM users WHERE id=${id};
+    `);
+    if (!user) throw { name: "UserNotFoundError", message: "User not found" };
+    delete user.password;
+    user.recipes = await getAllRecipesForUser(id);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllRecipesForUser(id) {
+  try {
+    const {
+      rows: [recipes],
+    } = await client.query(`
+      SELECT * FROM recipes WHERE user_id=${id};
+    `);
+    return recipes;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getRecipeInfo(recipeId) {
+  try {
+    const {
+      rows: [recipe],
+    } = await client.query(`
+      SELECT * FROM recipes WHERE id=${recipeId};
+    `);
+    if (!recipe)
+      throw { name: "RecipeNotFoundError", message: "Recipe not found" };
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createRecipe({
+  userId,
+  name,
+  ingredients,
+  units,
+  volUnits,
+  rowCount,
+  submitted,
+  yeastInfo,
+  yeastBrand,
+  nuteSchedule,
+  numOfAdditions,
+  extraIngredients,
+}) {
+  try {
+    const {
+      rows: [recipe],
+    } = await client.query(
+      `
+      INSERT INTO recipes (user_id, name, ingredients, units, vol_units, row_count, submitted, yeast_info, yeast_brand, nute_schedule, num_of_additions, extra_ingredients)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+      RETURNING *;
+    `,
+      [
+        userId,
+        name,
+        ingredients,
+        units,
+        volUnits,
+        rowCount,
+        submitted,
+        yeastInfo,
+        yeastBrand,
+        nuteSchedule,
+        numOfAdditions,
+        extraIngredients,
+      ]
+    );
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateRecipe(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [recipe],
+    } = await client.query(
+      `
+      UPDATE recipes
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+    `,
+      Object.values(fields)
+    );
+
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createIngredient({
+  name,
+  sugarContent,
+  waterContent,
+  category,
+}) {
+  try {
+    const {
+      rows: [ingredient],
+    } = await client.query(
+      `
+      INSERT INTO ingredients (name, sugar_content, water_content, category)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `,
+      [name, sugarContent, waterContent, category]
+    );
+    return ingredient;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateIngredient(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${key}"=$${index + 1}`
+  );
+  try {
+    const {
+      rows: [ingredient],
+    } = await client.query(
+      `
+    UPDATE ingredients
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+  `,
+      Object.values(fields)
+    );
+
+    return ingredient;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllIngredients() {
+  try {
+    const { rows: ingredients } = await client.query(`
+      SELECT * FROM ingredients;
+    `);
+    return ingredients;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getIngredient(id) {
+  try {
+    const {
+      rows: [ingredient],
+    } = await client.query(`
+      SELECT * FROM ingredients WHERE id=${id};
+    `);
+    if (!ingredient)
+      throw {
+        name: "IngredientNotFoundError",
+        message: "Ingredient not found",
+      };
+    return ingredient;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getIngredientsByCategory(category) {
+  try {
+    const {
+      rows: [ingredients],
+    } = await client.query(`
+     SELECT * FROM ingredients WHERE category=${category};
+    `);
+    return ingredients;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getIngredientByName(name) {
+  try {
+    const {
+      rows: [ingredient],
+    } = await client.query(`
+      SELECT * FROM ingredients WHERE name=${name};
+    `);
+    if (!ingredient)
+      throw {
+        name: "IngredientNotFoundError",
+        message: "Ingredient not found",
+      };
+    return ingredient;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createYeast({
+  brand,
+  name,
+  nitrogenRequirement,
+  tolerance,
+  lowTemp,
+  highTemp,
+}) {
+  try {
+    const {
+      rows: [yeast],
+    } = await client.query(
+      `
+      INSERT INTO yeasts (brand, name, nitrogen_requirement, tolerance, low_temp, high_temp)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING *;
+    `,
+      [brand, name, nitrogenRequirement, tolerance, lowTemp, highTemp]
+    );
+    return yeast;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateYeast(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${key}"=$${index + 1}`
+  );
+  try {
+    const {
+      rows: [yeast],
+    } = await client.query(
+      `
+    UPDATE yeasts
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+  `,
+      Object.values(fields)
+    );
+
+    return yeast;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllYeasts() {
+  try {
+    const { rows: yeasts } = await client.query(`
+      SELECT * FROM yeasts;
+    `);
+    return yeasts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getYeastByName(name) {
+  try {
+    const {
+      rows: [yeast],
+    } = await client.query(`
+      SELECT * FROM yeasts WHERE name=${name};
+    `);
+    if (!yeast)
+      throw {
+        name: "YeastNotFoundError",
+        message: "Yeast not found",
+      };
+    return yeast;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getYeastByBrand(brand) {
+  try {
+    const {
+      rows: [yeast],
+    } = await client.query(`
+      SELECT * FROM yeasts WHERE brand=${brand};
+    `);
+    if (!yeast)
+      throw {
+        name: "YeastNotFoundError",
+        message: "Yeast not found",
+      };
+    return yeast;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  client,
+  createUser,
+  getAllUsers,
+  getUser,
+  getAllRecipesForUser,
+  getRecipeInfo,
+  createRecipe,
+  updateRecipe,
+  createIngredient,
+  updateIngredient,
+  getAllIngredients,
+  getIngredient,
+  getIngredientsByCategory,
+  getIngredientByName,
+  createYeast,
+  updateYeast,
+  getAllYeasts,
+  getYeastByName,
+  getYeastByBrand,
+};
---
+++ db/seed.js
@@ -0,0 +1,238 @@
+const {
+  client,
+  createUser,
+  getAllUsers,
+  getUser,
+  getAllRecipesForUser,
+  getRecipeInfo,
+  createRecipe,
+  updateRecipe,
+  createIngredient,
+  updateIngredient,
+  getAllIngredients,
+  getIngredient,
+  getIngredientsByCategory,
+  getIngredientByName,
+  createYeast,
+  updateYeast,
+  getAllYeasts,
+  getYeastByName,
+  getYeastByBrand,
+} = require("./index");
+
+const INGREDIENTS = require("./fermentables.js");
+const YEASTS = require("./yeast.js");
+
+async function dropTables() {
+  try {
+    console.log("Starting drop tables");
+    await client.query(`
+    DROP TABLE IF EXISTS recipes;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS ingredients;
+    DROP TABLE IF EXISTS ingredient_categories;
+    DROP TABLE IF EXISTS yeasts;`);
+
+    console.log("Tables dropped");
+  } catch (error) {
+    console.error("Error while dropping tables");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Creating tables...");
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      first_name varchar(255),
+      last_name varchar(255),
+      username varchar(255) UNIQUE NOT NULL,
+      email varchar(255) NOT NULL,
+      password varchar(255) NOT NULL,
+      role varchar(255) DEFAULT 'user'
+    );
+
+    CREATE TABLE recipes (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(id),
+      name varchar(255) NOT NULL,
+      ingredients TEXT NOT NULL,
+      units varchar(255) NOT NULL,
+      vol_units varchar(255) NOT NULL,
+      row_count int NOT NULL,
+      submitted boolean DEFAULT true NOT NULL,
+      yeast_info TEXT NOT NULL,
+      yeast_brand varchar(255) NOT NULL,
+      nute_schedule varchar(255) NOT NULL,
+      num_of_additions int NOT NULL,
+      extra_ingredients TEXT NOT NULL
+    );
+
+    CREATE TABLE ingredients (
+      id SERIAL PRIMARY KEY,
+      name varchar(255) NOT NULL,
+      sugar_content numeric NOT NULL,
+      water_content numeric NOT NULL,
+      category varchar(255) NOT NULL
+    );
+
+    CREATE TABLE yeasts (
+      id SERIAL PRIMARY KEY,
+      brand varchar(255) NOT NULL,
+      name varchar(255) NOT NULL,
+      nitrogen_requirement varchar(255) NOT NULL,
+      tolerance numeric NOT NULL,
+      low_temp numeric NOT NULL,
+      high_temp numeric NOT NULL
+    );
+
+    `);
+    console.log("Tables created");
+  } catch (error) {
+    console.error("Error while creating tables");
+    throw error;
+  }
+}
+
+async function createInitialUsers() {
+  const ADMIN_USER = {
+    firstName: "Admin",
+    lastName: "User",
+    username: "admin",
+    email: "contact@meadtools.com",
+    password: "PASSWORD",
+    role: "admin",
+  };
+  const USER = {
+    firstName: "User",
+    lastName: "User",
+    username: "user",
+    email: "contact@meadtools.com",
+    password: "PASSWORD",
+    role: "user",
+  };
+  try {
+    await createUser(ADMIN_USER);
+    await createUser(USER);
+    console.log("Initial users created");
+  } catch (error) {
+    console.error("Error while creating initial users");
+    throw error;
+  }
+}
+
+async function createInitialRecipes() {
+  const ingredients = {
+    input1: {
+      name: "Honey",
+      weight: "3",
+      brix: 79.6,
+      volume: "0.254",
+      cat: "sugar",
+    },
+    input2: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input3: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input4: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input5: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input6: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input7: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input8: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input9: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input10: { name: "Honey", weight: 0, brix: 79.6, volume: 0, cat: "sugar" },
+    input11: {
+      name: "Water",
+      weight: "6.259",
+      brix: "0",
+      volume: ".75",
+      cat: "sugar",
+    },
+    input12: { name: "Water", weight: 0, brix: "0", volume: 0, cat: "sugar" },
+  };
+  const yeastInfo = [
+    {
+      name: "ICV D47",
+      "Nitrogen Requirement": "Low",
+      "ABV Tolerance": 17,
+      LowTemp: 50,
+      HighTemp: 86,
+    },
+  ];
+
+  const extra_ingredients = {
+    input1: { name: "Opti-White", amount: "1.1", units: "g" },
+    input2: { name: "", amount: 0, units: "g" },
+    input3: { name: "", amount: 0, units: "g" },
+    input4: { name: "", amount: 0, units: "g" },
+    input5: { name: "", amount: 0, units: "g" },
+  };
+
+  const TRADITIONAL_MEAD = {
+    userId: 1,
+    name: "Traditional Mead",
+    ingredients: JSON.stringify(ingredients),
+    units: "lbs",
+    volUnits: "gal",
+    rowCount: 0,
+    submitted: true,
+    yeastInfo: JSON.stringify(yeastInfo),
+    yeastBrand: "Lalvin",
+    nuteSchedule: "tosna",
+    numOfAdditions: 3,
+    extraIngredients: JSON.stringify(extra_ingredients),
+  };
+  try {
+    console.log("Creating initial recipes...");
+    await createRecipe(TRADITIONAL_MEAD);
+    console.log("Initial recipes created");
+  } catch (error) {
+    console.error("Error while creating initial recipes");
+    throw error;
+  }
+}
+
+async function createInitialIngredients() {
+  try {
+    console.log("Creating initial ingredients...");
+    INGREDIENTS.forEach(async (ingredient) => {
+      await createIngredient(ingredient);
+    });
+    console.log("Initial ingredients created");
+  } catch (error) {
+    console.error("Error while creating initial ingredients");
+    throw error;
+  }
+}
+
+async function createInitialYeasts() {
+  for (const [key, value] of Object.entries(YEASTS)) {
+    const values = value.forEach(async (yeast) => {
+      await createYeast({ ...yeast, brand: key });
+    });
+
+    console.log(values);
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+    await createInitialRecipes();
+    await createInitialIngredients();
+    await createInitialYeasts();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+// async function testDB() {
+
+// }
+
+rebuildDB();
