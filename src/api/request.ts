3572af47d77042744ae88ba9060137b7be38a67c
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-04-22 17:12:52 -0500

    edit uri

--- src/api/request.ts
+++ src/api/request.ts
@@ -5,7 +5,7 @@ import { OAuth2Client } from "google-auth-library";
 requestRouter.post("/", async function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Referrer-Policy", "no-referrer-when-downgrade");
-  const redirectUrl = "https://mead-tools-api.vercel.app/users/oauth";
+  const redirectUrl = "https://mead-tools-api.vercel.app/api/users/oauth";
 
   const oAuth2Client = new OAuth2Client(
     process.env.GOOGLE_CLIENT_ID,
