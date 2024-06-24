a080a303fadef1ec63731512e1a715e7ff96e47c
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-06-24 18:10:32 -0500

    checking full url

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,9 +2,11 @@ import express from "express";
 const requestRouter = express.Router();
 import { OAuth2Client } from "google-auth-library";
 
-requestRouter.post("/", async function (_, res) {
+requestRouter.post("/", async function (req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Referrer-Policy", "no-referrer-when-downgrade");
+  let fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+  console.log(fullUrl);
   const redirectUrl = "https://mead-tools-api.vercel.app/api/users/oauth";
 
   const oAuth2Client = new OAuth2Client(
