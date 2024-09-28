aa24700f24a1fc9f195ba3565577b5a995813ede
ljreaux <larryreaux@gmail.com>
2024-09-28 11:05:28 -0500

    added new routes

--- dist/api/iSpindel.js
+++ dist/api/iSpindel.js
@@ -4,12 +4,74 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const utils_1 = require("./utils");
 const iSpindelRouter = express_1.default.Router();
+const short_unique_id_1 = __importDefault(require("short-unique-id"));
+iSpindelRouter.get("/", utils_1.requireUser, async (req, res, next) => {
+    try {
+        const { id: userId } = req.user || { id: null };
+        const { body } = req;
+        console.log(body);
+        res.send(`Fetching deviceList for user ${userId}`);
+    }
+    catch (err) {
+        next({ error: err.message });
+    }
+});
 iSpindelRouter.post("/", async (req, res, next) => {
     try {
         const { body } = req;
         console.log(body);
         res.send("iSpindel recipe created successfully!");
+    }
+    catch (err) {
+        next({ error: err.message });
+    }
+});
+iSpindelRouter.get("/logs", utils_1.requireUser, async (req, res, next) => {
+    try {
+        const queryParams = req.query;
+        const { id: userId } = req.user || { id: null };
+        const { body } = req;
+        console.log(body);
+        res.send(`Fetching the ${queryParams} most recent logs for user ${userId}`);
+    }
+    catch (err) {
+        next({ error: err.message });
+    }
+});
+iSpindelRouter.get("/logs/:brewId", utils_1.requireUser, async (req, res, next) => {
+    try {
+        const { brewId } = req.params;
+        const { id: userId } = req.user || { id: null };
+        const { body } = req;
+        console.log(body);
+        res.send(`Fetching user ${userId} logs for brew ${brewId}`);
+    }
+    catch (err) {
+        next({ error: err.message });
+    }
+});
+iSpindelRouter.get("/:deviceId", utils_1.requireUser, async (req, res, next) => {
+    try {
+        const { id: userId } = req.user || { id: null };
+        const { deviceId } = req.params;
+        const { body } = req;
+        console.log(body);
+        res.send(`Fetching recent logs for device ${deviceId} from user ${userId}`);
+    }
+    catch (err) {
+        next({ error: err.message });
+    }
+});
+iSpindelRouter.post("/register", utils_1.requireUser, async (req, res, next) => {
+    try {
+        const { id: userId } = req.user || { id: null };
+        const { body } = req;
+        console.log(body);
+        const { randomUUID } = new short_unique_id_1.default();
+        const token = randomUUID(10);
+        res.send(`Generating a new token for ${userId}. Your token is ${token}`);
     }
     catch (err) {
         next({ error: err.message });
--- src/api/iSpindel.ts
+++ src/api/iSpindel.ts
@@ -1,5 +1,19 @@
 import express from 'express';
+import { requireUser } from './utils';
+import { UserAuthInfoRequest } from '.';
 const iSpindelRouter = express.Router();
+import ShortUniqueId from 'short-unique-id'
+
+iSpindelRouter.get("/", requireUser, async (req: UserAuthInfoRequest, res, next) => {
+  try {
+    const { id: userId } = req.user || { id: null };
+    const { body } = req;
+    console.log(body);
+    res.send(`Fetching deviceList for user ${userId}`);
+  } catch (err) {
+    next({ error: err.message })
+  }
+})
 
 iSpindelRouter.post("/", async (req, res, next) => {
   try {
@@ -9,6 +23,56 @@ iSpindelRouter.post("/", async (req, res, next) => {
   } catch (err) {
     next({ error: err.message })
   }
+});
+
+iSpindelRouter.get("/logs", requireUser, async (req: UserAuthInfoRequest, res, next) => {
+  try {
+    const queryParams = req.query
+    const { id: userId } = req.user || { id: null };
+    const { body } = req;
+    console.log(body);
+    res.send(`Fetching the ${queryParams} most recent logs for user ${userId}`);
+  } catch (err) {
+    next({ error: err.message })
+  }
+});
+iSpindelRouter.get("/logs/:brewId", requireUser, async (req: UserAuthInfoRequest, res, next) => {
+  try {
+    const { brewId } = req.params
+    const { id: userId } = req.user || { id: null };
+    const { body } = req;
+    console.log(body);
+    res.send(`Fetching user ${userId} logs for brew ${brewId}`);
+  } catch (err) {
+    next({ error: err.message })
+  }
+});
+
+iSpindelRouter.get("/:deviceId", requireUser, async (req: UserAuthInfoRequest, res, next) => {
+  try {
+    const { id: userId } = req.user || { id: null };
+    const { deviceId } = req.params;
+    const { body } = req;
+    console.log(body);
+    res.send(`Fetching recent logs for device ${deviceId} from user ${userId}`);
+  } catch (err) {
+    next({ error: err.message })
+  }
 })
+
+iSpindelRouter.post("/register", requireUser, async (req: UserAuthInfoRequest, res, next) => {
+  try {
+    const { id: userId } = req.user || { id: null };
+    const { body } = req;
+    console.log(body);
+
+    const { randomUUID } = new ShortUniqueId();
+    const token = randomUUID(10);
+
+    res.send(`Generating a new token for ${userId}. Your token is ${token}`);
+  } catch (err) {
+    next({ error: err.message })
+  }
+});
 
 export default iSpindelRouter
